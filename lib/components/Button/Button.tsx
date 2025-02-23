4207d7f6ee078b2dd508b05590b00ab72dbdf9c0
Amandeep Kirar <ak4wrapp@gmail.com>
2025-02-23 02:33:34 -0500

    storybook onboarding

--- lib/components/Button/Button.tsx
+++ lib/components/Button/Button.tsx
@@ -1,21 +1,14 @@
-// import styles from "./Button.module.css"; // Import CSS module
-
-// const Button = () => {
-//   return <button className={styles.button}>Button</button>;
-// };
-
-// export default Button;
-
 import React from "react";
 import styles from "./Button.module.css"; // Import CSS module
 
-interface MyButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+export interface MyButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   theme?: "dark" | "light" | "primary" | "secondary";
 }
 
-const Button: React.FC<MyButtonProps> = ({ theme, children }) => {
+const Button: React.FC<MyButtonProps> = ({ theme, children, ...props }) => {
   return (
-    <button className={styles.button} data-theme={theme}>
+    <button className={styles.myButton} data-theme={theme} {...props}>
       {children}
     </button>
   );
